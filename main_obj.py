b8275c3518d4d54d559ebc1f5dd1b7b1c502f9a2
Pawnzr <anduu150620@gmail.com>
2024-05-02 15:16:12 +0700

    access data on db

--- main_obj.py
+++ main_obj.py
@@ -1,50 +1,11 @@
 import telebot
+from target import Target
 from app import create_directory, read_file
-import subprocess
-import os
 from env_var import authorized_users, TOKEN
-from sqlalchemy import create_engine, Column, Integer, String, Text
+import sqlite3
 
 authorized_users = authorized_users
-
 bot = telebot.TeleBot(TOKEN)
-
-
-class Target:
-    def __init__(self, url):
-        self.url = url
-        self.subdomain_results = None
-        self.httpx_results = None
-
-    def scan_subdomains(self):
-        result_path = f"./Result/{self.url}"
-        result_file = f"{result_path}/subdomain.txt"
-
-        if not os.path.exists(result_path):
-            create_directory(result_path)
-
-            subdomain_cmd = f"subfinder -d {self.url} > {result_file}"
-            subprocess.run(subdomain_cmd, shell=True)
-
-        with open(result_file, 'r') as f:
-            self.subdomain_results = f.read().strip() or "Không tìm thấy subdomain"
-
-    def scan_httpx(self):
-        result_file = f"./Result/{self.url}/httpx.txt"
-        if self.subdomain_results is None:
-            self.scan_subdomains()
-        if self.httpx_results is None:
-            httpx_cmd = f"httpx -l ./Result/{self.url}/subdomain.txt -nc --no-fallback -tls-probe -csp-probe -title -vhost -td -status-code -fr -cdn -random-agent > {result_file}"
-            subprocess.run(httpx_cmd, shell=True)
-
-        with open(result_file, 'r') as f:
-            self.httpx_results = f.read().strip() or "Không tìm thấy httpx"
-
-    def get_subdomain_results(self):
-        return self.subdomain_results
-
-    def get_httpx_results(self):
-        return self.httpx_results
 
 
 @bot.message_handler(commands=['help', 'start'])
@@ -53,28 +14,38 @@ def handle_start(message):
 
 @bot.message_handler(commands=['subdomain'])
 def handle_subdomain(message):
-    if message.chat.id in authorized_users:
+    conn = sqlite3.connect('mydatabase.db')
+    if str(message.chat.id) in authorized_users:
         target = message.text.split()[1]
         target_obj = Target(target)
-        target_obj.scan_subdomains()
-        subdomain_results = target_obj.get_subdomain_results()
-        bot.reply_to(message, subdomain_results)
+        subdomains = target_obj.get_subdomain()
+        if subdomains:
+            bot.reply_to(message, subdomains)
+        else:
+            target_obj.scan_subdomains()
+            subdomains = target_obj.get_subdomain()
+            bot.reply_to(message, subdomains)
+        target_obj.save_to_sqlite(conn)
     else:
         bot.reply_to(message, "Bạn không được phép sử dụng bot này.")
 
 
 @bot.message_handler(commands=['httpx'])
 def handle_httpx(message):
+    conn = sqlite3.connect('mydatabase.db')
     if str(message.chat.id) in authorized_users:
         target = message.text.split()[1]
         target_obj = Target(target)
+        if target_obj.httpx:
+            bot.reply_to(message, target.httpx)
         target_obj.scan_httpx()
-        subdomain_results = target_obj.get_subdomain_results()
+        subdomain_results = target_obj.get_subdomain()
         bot.reply_to(message, subdomain_results)
         # subdomain_results = target_obj.get_subdomain_results()
-        # httpx_results = target_obj.get_httpx_results()
+        # httpx_results = target_obj.get_httpx()
         bot.reply_to(message, subdomain_results)
         # bot.reply_to(message, httpx_results)
+        target_obj.save_to_sqlite(conn)
     else:
         bot.reply_to(message, "Bạn không được phép sử dụng bot này.")
 
@@ -86,13 +57,12 @@ def handle_scan(message):
         target_obj = Target(target)
         bot.reply_to(message, f'Bắt đầu scan {target}')
         target_obj.scan_subdomains()
-        subdomain_results = target_obj.get_subdomain_results()
+        subdomain_results = target_obj.get_subdomain()
         bot.reply_to(message, f"Kết quả subdomain:\n{subdomain_results}")
         target_obj.scan_httpx()
-        httpx_results = target_obj.get_httpx_results()
+        httpx_results = target_obj.get_httpx()
         bot.reply_to(message, f"Kết quả httpx:\n{httpx_results}")
     except ValueError:
         bot.reply_to(message, 'Sai định dạng số. Vui lòng nhập hai số hợp lệ.')
-
 
 bot.infinity_polling()
--- target.py
+++ target.py
@@ -1,41 +1,62 @@
-from app import *
+from app import create_directory,  read_file
 import subprocess
-
+import os
 class Target:
     def __init__(self, url):
         self.url = url
-        self.subdomain = None
+        self.subdomains = None
         self.httpx = None
-        self.port = None
-    
+        self.ports = None
+        
+    def save_to_sqlite(self, conn):
+        # Create the table if it doesn't exist
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS targets (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    url TEXT UNIQUE,
+                    subdomains TEXT,
+                    ports TEXT,
+                    httpx TEXT
+                    )''')
+
+        # Insert data into the table
+        c.execute("INSERT INTO targets (url, subdomains, httpx, ports ) VALUES (?, ?, ?, ?)",
+                  (self.url, self.subdomains, self.httpx, self.ports))
+        conn.commit()
+        
     def scan_subdomains(self):
-        result_path = f"./Result/{self.url}"
-        result_file = f"{result_path}/subdomain.txt"
-        
-        if not os.path.exists(result_path):
-            create_directory(result_path)
-        
-        subdomain_cmd = f"subfinder -d {self.url} > {result_file}"
-        subprocess.run(subdomain_cmd, shell=True)
-        
-        with open(result_file, 'r') as f:
-            self.subdomain_results = f.read().strip() or "Không tìm thấy subdomain"
+        try:
+            result_path = f"./Result/{self.url}"
+            result_file = f"{result_path}/subdomains.txt"
+
+            if not os.path.exists(result_path):
+                create_directory(result_path)
+                subdomains_cmd = f"subfinder -d {self.url} > {result_file}"
+                subprocess.run(subdomains_cmd, shell=True)
+
+            with open(result_file, 'r') as f:
+                self.subdomains = f.read().strip() or "Không tìm thấy subdomains"
+        except:
+            self.subdomains = "Không tìm thấy subdomains"
+
     def scan_httpx(self):
-        if self.subdomain_results is None:
-            self.scan_subdomains()
-        
-        result_file = f"./Result/{self.url}/httpx.txt"
-        httpx_cmd = f"httpx -l ./Result/{self.url}/subdomain.txt -nc --no-fallback -tls-probe -csp-probe -title -vhost -td -status-code -fr -cdn -random-agent > {result_file}"
-        subprocess.run(httpx_cmd, shell=True)
-        
-        with open(result_file, 'r') as f:
-            self.httpx_results = f.read().strip() or "Không tìm thấy httpx"
+        try:
+            result_file = f"./Result/{self.url}/httpx.txt"
+            if self.subdomains is None:
+                self.scan_subdomains()
+            if self.httpx is None:
+                httpx_cmd = f"httpx -l ./Result/{self.url}/subdomain.txt -nc --no-fallback -tls-probe -csp-probe -title -vhost -td -status-code -fr -cdn -random-agent > {result_file}"
+                subprocess.run(httpx_cmd, shell=True)
 
-    def get_subdomain_results(self):
-        return self.subdomain
+            with open(result_file, 'r') as f:
+                self.httpx = f.read().strip() or "Không tìm thấy Httpx"
+        except:
+            self.httpx = "Lỗi khi chạy Httpx"
 
-    def get_httpx_results(self):
+    def get_subdomain(self):
+        return self.subdomains
+
+    def get_httpx(self):
         return self.httpx
     
-    def get_port(self):
-        return self.port
+    
