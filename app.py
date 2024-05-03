cbc56cb7b63e382fd0deeec5cfa0d73265732de8
Pawnzr <anduu150620@gmail.com>
2024-05-03 14:24:32 +0700

    update workflow and function

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import os
 import errno
 import subprocess
+import socket
+import xml.etree.ElementTree as ET
+
+def check_file_exists(filepath):
+    if os.path.exists(filepath):
+        return True
+    else:
+        return False
 
 def create_directory(target_dir):
+    
     try:
         os.makedirs(target_dir)
     except OSError as e:
@@ -25,3 +34,72 @@ def scan_subdomain(target):
     with open(result_file, 'r') as f:
         subdomains = f.read().strip() or target
     return subdomains
+
+def scan_port(target):
+    nmap_export_path = f"Result/{target}"
+    create_directory(nmap_export_path)
+    nmap_export_file = f'Result/{target}/nmap.xml'
+    if check_file_exists(nmap_export_file):
+        data = export_data(nmap_export_file)
+        return data
+    else:
+        try:
+            nmap_cmd = f"nmap -sV -Pn -T4 {target} -oX {nmap_export_file}"
+            subprocess.run(nmap_cmd, shell=True)
+            data = export_data(nmap_export_file)
+            return data
+        except:
+            return ""
+    
+def resolve_domain_to_ip(domain):
+    try:
+        ip = socket.gethostbyname(domain)
+        return ip
+    except:
+        return ""
+
+def resolve_ip_to_domain(ip):
+    try:
+        return socket.gethostbyaddr(ip)[0]
+    except:
+        return ""
+    
+def export_data(data_path):
+    port_result = []
+    with open(f"{data_path}",'r') as f:
+        data = f.read()
+    root = ET.fromstring(data)
+    port_id = ''
+    protocol = ''
+    state = ''
+    service_name = ''
+    service_product = ''
+    service_version = ''
+    hostname = ''
+    service_servicefp = ''
+    service_service_extra = ''
+    try:
+        for port in root.findall('.//port'):
+            port_id = port.get('portid') or ''
+            protocol = port.get('protocol') or ''
+            state = port.find('.//state').get('state') or ''
+            service_name = port.find('.//service').get('name') or ''
+            service_product = port.find('.//service').get('product') or ''
+            service_version = port.find('.//service').get('version') or ''
+            hostname = port.find('.//service').get('hostname') or ''
+            service_servicefp = port.find('.//service').get('servicefp') or ''
+            service_service_extra = port.find('.//service').get('extrainfo') or ''
+            version = service_product + ' ' + service_version
+            port_data = {
+                "port" : port_id,
+                "protocol" : protocol,
+                "state" : state,
+                "service": service_name,
+                "version" : version,
+                "extra_info" :  {"hostname": hostname,"servicefp":service_servicefp,"extrainfo":service_service_extra}
+            }
+            
+            port_result.append(port_data)
+        return port_result
+    except:
+        return ""
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from target_manager import Target, TargetManager
 from env_var import authorized_users, TOKEN
 import sqlite3
 import subprocess
-from app import create_directory, scan_subdomain
+from app import *
 authorized_users = authorized_users
 bot = telebot.TeleBot(TOKEN)
 db_path = "database.db"
@@ -17,17 +17,34 @@ def handle_scan(message):
     
     if str(message.chat.id) in authorized_users:
         target = message.text.split()[1]
-        target_obj = Target("",target,"","","")
+        ip = resolve_domain_to_ip(target)
+        
+        target_obj = Target(ip,target,"","","")
+        
         manager = TargetManager(db_path)
         manager.create_table()
         manager.add_target(target_obj)
         targets = manager.get_target_by_domain(target)
+        
         if targets[3] == "":
             subdomains = scan_subdomain(target)
             manager.update_target_subdomain_by_domain(target, subdomains)
         else:
             subdomains = manager.get_target_by_domain(target)[3]
-        
+
+        if targets[2] == "":
+            ports = scan_port(ip)
+            manager.update_target_port_by_ip(ip, str(ports))
+        else:
+            ports = manager.get_target_by_domain(target)[2]
+
+        bot.reply_to(message,targets)
+
+        # for subdomain in subdomains.split("\n"):
+        #     ip = resolve_domain_to_ip(subdomain)
+        #     target_object = Target(ip,subdomain,"","","")
+        #     manager.add_target(target_object)
+            
 
         
 bot.infinity_polling()
